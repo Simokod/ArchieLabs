ae6b33004551a7612cbcb7982988047d6ad55abc
Simokod <44751590+Simokod@users.noreply.github.com>
2019-03-05 10:37:46 +0200

    Add files via upload

--- encoder.c
+++ encoder.c
@@ -1,40 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void encodeDebug();
 
 int main(int argc, char **argv) 
 {
-    if(strcmp(argv[1], "-D")==0)
-        encodeDebug();
-        else {
-            char c;
-            c = fgetc(stdin);
-            while(c!=EOF) {
-                if((c>64) & (c<91))
-                    fputc(c+32, stdout);
-                else
-                    fputc(c, stdout);
-                c=fgetc(stdin);
-            }
-            printf("\n");
-        }
-}
+    // args params
+    int i, j, isDebug=0, isEncrpt=0, isPlus=0, isRead=0;  
+    // encrypt params
+    int encIndex=2, encArgIndex;
+    // reading parameters
+    int readArgIndex;
 
-void encodeDebug() {
-    char c;
+    int c;
+    // param check
+    for(i=1;i<argc;i++){
+        if(strcmp(argv[i], "-D")==0){
+            if(!isDebug){
+                for(j=1;j<argc;j++)
+                    puts(argv[j]);
+                isDebug=1;
+            }
+        }
+        if(strncmp(argv[i], "+e", 2)==0){
+            isEncrpt = 1;
+            isPlus = 1;
+            encArgIndex = i;
+        }
+        if(strncmp(argv[i], "-e", 2)==0){
+            isEncrpt = 1;
+            encArgIndex = i;
+        }
+        if(strncmp(argc[i],"-i",2)==0){
+            isRead = 1;
+            readArgIndex = i;
+        }
+    }
+
     c = fgetc(stdin);
     while(c!=EOF) {
-        fprintf(stderr, "0x%x\t", c);
-        if((c>64) & (c<91)){
-            fprintf(stderr, "0x%x\n", c+32);
-            fputc(c+32, stdout);
+        if(isDebug){
+            fprintf(stderr, "0x%x\t", c);
         }
-        else{
-            fprintf(stderr, "0x%x\n", c);
-            fputc(c, stdout);
+
+        if(isEncrpt){   // encryption
+            if(isPlus){ // +e
+                fputc(c + argv[encArgIndex][encIndex], stdout);
+                if(isDebug){
+                    fprintf(stderr, "0x%x\n", c+argv[encArgIndex][encIndex]);
+                }
+            }
+            else {  // -e
+                fputc(c - argv[encArgIndex][encIndex], stdout);
+                if(isDebug)
+                    fprintf(stderr, "0x%x\n", c-argv[encArgIndex][encIndex]);
+            }
+
+            ++encIndex;
+            if (encIndex>strlen(argv[encArgIndex])-1)
+                encIndex = 2;
+            if(c == '\n'){
+                encIndex=2;
+                fputc('\n', stdout);
+            }
+        }
+
+        else{       // no encryption
+            if((c>='A') & (c<='Z')){
+                if(isDebug){
+                    fprintf(stderr, "0x%x\n", c+'a'-'A');
+                }
+                fputc(c+'a'-'A', stdout);
+            }
+            else {
+                if(isDebug){
+                    fprintf(stderr, "0x%x\n", c);
+                }
+                fputc(c, stdout);
+            }
         }
         c=fgetc(stdin);
     }
-    printf("\n");
 }
