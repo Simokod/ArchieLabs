e7a1a24c8eb34ae74b8f581ba67dd1a43bf71e66
Simokod <44751590+Simokod@users.noreply.github.com>
2019-04-11 12:30:58 +0300

    Update calc.s

--- ArchieAss2/calc.s
+++ ArchieAss2/calc.s
@@ -1,19 +1,16 @@
 section	.rodata						
 	format_string: db "%s", 10, 0										; format string
 	format_number: db "%d", 10, 0										; format number
-	prompt_str: db "calc: " , 10 , 0											
-	operand_error: db "Error: Operand Stack Overflow"					
-	args_error: db "Error: Insufficient Number of Arguments on Stack"
-	stdin: dd 0			; sdtin file discreption
-	stdout: dd 1		; sdtout file discreption
-	stderr: dd 2		; sdterr file discreption
-	
+	prompt_str: db "calc: " , 10, 0											
+	operand_error: db "Error: Operand Stack Overflow", 10, 0
+	args_error: db "Error: Insufficient Number of Arguments on Stack", 10, 0
+
 section .bss
     buffer: resb 80            		; storing input
+	stack: resd 5					; allocating 20 bytes for 5 pointers
 
 section .data
-	result: dd 0
-	stack: dd 5						; allocating 20 bytes for 5 pointers
+	size_of_stack: db 0				; amount of operands in the stack
 	num_of_ops: dd 0				; total number of operations
 
 section .text
@@ -27,54 +24,70 @@ section .text
 	extern free 
 	extern gets 
 	extern fgets 
+	extern stdin
+	extern stdout
+	extern stderr
 
-main: 
-	pushad
-	pushfd
-
+main:
 	call mycalc					; actual function
 	
-	popad
-	popfd
-
 	push eax					; pushing number of operations on Stack
 	push format_number			; pushing format_number
 	call printf					; printing 
+	add esp, 8					; freeing func memory
 
-	mov esp, ebp 				; freeing func AF
-	pop ebp 					; restore AF of main
 	ret
 
 mycalc:
-	.loop:
+	push ebp					; backup last activation frame
+	mov ebp, esp				; save current activation frame
+	pushad						; backup registers
+
+	loop:
 		push prompt_str
 		call printf
 		add esp, 4						; remove pushed argument
 
 		push dword [stdin]              ; reading from stdin
-		push dword 80                   ; max length
-		push dword buffer               ; input buffer
+		push 80                   		; max length
+		push buffer               		; input buffer
 		call fgets						; reading input
 		add esp, 12                     ; remove args from stack
 
-		cmp eax, "q"				
-		je .q_case
-;		cmp eax, "+"
+switch:
+		cmp byte [buffer], 'q'				
+		je q_case
+;		cmp byte [buffer], "+"
 ;		je add_case
-;		cmp eax, "p"
+;		cmp byte [buffer], "p"
 ;		je pop_and_print
-;;		cmp eax, "d"
+;		cmp byte [buffer], "d"
 ;		je duplicate
-;		cmp eax, "^"
+;		cmp byte [buffer], "^"
 ;		je power
-;		cmp eax, "v"
+;		cmp byte [buffer], "v"
 ;		je negative_power
-;;		cmp eax, "n"
+;		cmp byte [buffer], "n"
 ;		je one_bits
 
-;		.operand:
+		operand:
+			mov ecx, eax				; moving pointer to buffer to ecx
 
-		.q_case:
+			push 1						; size of char for calloc
+			push 5						; num of bytes to callocate (1 for digits + 4 for pointer)
+			call calloc					; allocating memory for churrent two digits
+			add esp, 8					; remove pushed argument
+
+			mov dword [stack + 4*size_of_stack], eax	; storing poiter to linked list in the stack
+			inc byte [size_of_stack]					; increasing amount of items in the stack
+
+			cmp dword [buffer], 0xA		; check if finished reading the number
+			jne operand_loop			; if not - return to loop
+
+			operand_loop:
+
+
+		q_case:
 			mov eax, [num_of_ops]		; return value
 																			; TODO: free allocated memory
 			mov esp, ebp 				; freeing func AF
