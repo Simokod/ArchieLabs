dcca4c716444aff4d96a511c8241258d41977de0
Simokod <44751590+Simokod@users.noreply.github.com>
2019-05-05 18:44:17 +0300

    Update looper.c

--- Lab5/looper.c
+++ Lab5/looper.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
+#include <string.h>
 
-void sigtstp_handler(int signum){
-    printf("Looper handling %s\n", strsignal(signum));
-    signal(signum, SIG_DFL);
-    signum(SIGCONT, sigcont_handler);
-    raise(SIGTSTP);
-}
-
-void sigcont_handler(int signum){
-    printf("Looper handling %s\n", strsignal(signum));
-    signal(signum, SIG_DFL);
-    signum(SIGTSTP, sigtstp_handler);
-    raise(SIGCONT);
+void sig_handler(int signum){
+    if(signum == SIGTSTP){
+	    printf("Looper handling SIGTSTP\n");
+	    signal(SIGTSTP, SIG_DFL);
+	    signal(SIGCONT, sig_handler);
+	    raise(SIGTSTP);
+	}
+	else {
+	    printf("Looper handling SIGCONT\n");
+	    signal(SIGCONT, SIG_DFL);
+	    signal(SIGTSTP, sig_handler);
+	    raise(SIGCONT);
+	}
 }
 
 void sigint_handler(int signum){
-	printf("exitting\n");
+	printf("Looper handling SIGINT\n");
 	_exit(1);
 }
 
 int main(int argc, char **argv){ 
 	signal(SIGINT, sigint_handler);
-	signal(SIGTSTP, sigtstp_handler);
-	signal(SIGCONT, sigcont_handler);
+	signal(SIGTSTP, sig_handler);
+	signal(SIGCONT, sig_handler);
 	printf("Starting the program\n");
 
 	while(1) {
